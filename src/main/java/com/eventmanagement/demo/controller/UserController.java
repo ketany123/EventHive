82951387af729345cbabc5f3bee1a154e7865d3c
ketany123 <ketanyelmate4@gmail.com>
2025-02-05 13:28:06 +0530

    Updated

--- src/main/java/com/eventmanagement/demo/controller/UserController.java
+++ src/main/java/com/eventmanagement/demo/controller/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController  // Add this annotation
+@RestController
 public class UserController {
     @Autowired
    private UserService userService;
